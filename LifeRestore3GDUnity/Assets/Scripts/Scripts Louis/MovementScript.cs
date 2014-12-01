b095775c4376d3ea9ae80aaa9315eee7a50246f7
Virgule222 <virgilelions@hotmail.fr>
2014-12-01 18:51:40 +0100

    1 Dec : >Ajout de script Dash (script movement) >Script pour afficher lien (pas encore intégré au proto)

--- LifeRestore3GDUnity/Assets/Scripts/Scripts Louis/MovementScript.cs
+++ LifeRestore3GDUnity/Assets/Scripts/Scripts Louis/MovementScript.cs
@@ -12,13 +12,36 @@ public class MovementScript : MonoBehaviour {
 
 	private float _movementSpeed;
 
+	//Dash 
+	private float _DashTimer;
+	private float _DashTimerCD;
+
+	private float _DashDuree;
+	private float _DashCD;
+	private bool _StopDash;
+
+
+	//Delegate pour enlever et remettre 
+	//delegate void Mydelegate();
+	//Mydelegate Actions ;
+
 	// Use this for initialization
 	void Start () {
 		_movementSpeed=10f;
+
+		_DashDuree = 0.3f;
+		_DashTimer = _DashDuree;
+		_DashCD = 0.5f;
+		_DashTimerCD = _DashCD;
+		_StopDash = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//Timer du CD du dash qui décroit 
+		if(_DashTimer> -1.0f){_DashTimer -= 1.0f*Time.deltaTime;}
+		if(_DashTimerCD> -1.0f){_DashTimerCD -= 1.0f*Time.deltaTime;}
+
 //		if (!playerIndexSet || !prevState.IsConnected)
 //		{
 //			for (int i = 0; i < 4; ++i)
@@ -33,16 +56,44 @@ public class MovementScript : MonoBehaviour {
 //				}
 //			}
 //		}
-
-
 		prevState = state;
 		state = GamePad.GetState(playerIndex);
 
+		//Ajout d'un rigidbody
+		if(rigidbody == null){
+			gameObject.AddComponent<Rigidbody>();
+			gameObject.rigidbody.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotationX|RigidbodyConstraints.FreezeRotationZ;
+		}
+		//Appui sur X = Dash 
+		if (_DashTimer <= 0.0f) {
+			if (_StopDash == true){gameObject.rigidbody.velocity = Vector3.zero; _StopDash = false;}
+			Movement ();
+			gameObject.rigidbody.mass = 1.0f;
+		}
+		if (prevState.Buttons.X == ButtonState.Released && state.Buttons.X == ButtonState.Pressed) {
+			if(_DashTimerCD <= 0.0f){
+				//Durant le dash on peut faire des tests colliders pour envoyer les gens plus loin, arreter le dash avant son intérgité etc.
+				Dash ();
+				//AR = (state.ThumbSticks.Left.X * _movementSpeed , 0.0f, state.ThumbSticks.Left.Y * _movementSpeed );
+				_DashTimerCD =_DashCD;
+			}else {Debug.Log("dash non dispo");}
+		}
+	}
+	//Movement de Base avec joystick
+	void Movement(){
+		
 		Vector3 movement = new Vector3(state.ThumbSticks.Left.X * _movementSpeed * Time.deltaTime, 0.0f, state.ThumbSticks.Left.Y * _movementSpeed * Time.deltaTime );
 		transform.localPosition += movement;
 		
 		transform.eulerAngles=new Vector3(0,0,transform.eulerAngles.z);
-
 	}
-	
+
+	//Action de dash 
+	void Dash(){
+		//possibilité de changer la hitbox pour faciliter le renre dedans
+		gameObject.rigidbody.mass = 500.0f;
+		gameObject.rigidbody.AddForce (new Vector3(state.ThumbSticks.Left.X * _movementSpeed , 0.0f, state.ThumbSticks.Left.Y * _movementSpeed)*1000.0f, ForceMode.Impulse);
+		_StopDash = true;
+		_DashTimer = _DashDuree;
+	}
 }
--- LifeRestore3GDUnity/Assets/Scripts/Scripts Virgile/DrawLink.cs
+++ LifeRestore3GDUnity/Assets/Scripts/Scripts Virgile/DrawLink.cs
@@ -3,18 +3,37 @@ using System.Collections;
 
 public class DrawLink : MonoBehaviour {
 
-	private PlayerState _State;
-	public GameObject _Idole;
+	//Script pour dessiner une ligne (pour les nuls)
+
+	private Color _Red = Color.red;
+	private Color _blue = Color.blue;
+	public GameObject _Player;
 
 	// Use this for initialization
 	void Start () {
-		_State = GetComponent<PlayerState> ();
+		//LineRenderer permet de créer une ligne entre 2 ou + points (la ligne n'est pas forcément droite de ce fait)
+		LineRenderer _link = gameObject.AddComponent<LineRenderer> ();
+		//On peut utiliser n'importe quel material pour rendre la ligne (on peut faire des trucs illuminés ou autres)
+		//Ceci dit avec un diffuse la couleur est noire problème de lumière surement je verrais 
+		_link.material = new Material (Shader.Find ("Particles/Additive"));
+		//On peut définir les couleurs, eh oui "les"
+		//Une pour le début de la ligne et une pour la fin (sur chaque segment)
+		_link.SetColors (_Red, _blue);
+		//Largeur de la ligne, moyen de la changer en gametime, neat!
+		_link.SetWidth (0.2f, 0.2f);
+		//Ca c'est le nombre de vertexs par lequels la ligne va passer
+		//Dans notre cas on veut entre deux objets donc A>B = 2 vertexs. on pourra ajouter des vertexs pour "relier" d'autres objets (Array)
+		_link.SetVertexCount (2);
+		//(sais pas pourquoi cette ligne mais on fait avec)
+		_link =  gameObject.AddComponent<LineRenderer> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(_State.v_isLinked){
-			Debug.DrawLine(_Idole.transform.position, transform.position, Color.blue);
-		}
+		LineRenderer _link = GetComponent<LineRenderer> ();
+		//On ne peut pas définir le point d'arrivée et de fin de la ligne à la place de ça on signal où se situe chaque point de la ligne
+		//Surement moyen de faire un tableau ou un truc pour automatiser la chose
+		_link.SetPosition (0, gameObject.transform.position);
+		_link.SetPosition (1, _Player.transform.position);
 	}
 }
