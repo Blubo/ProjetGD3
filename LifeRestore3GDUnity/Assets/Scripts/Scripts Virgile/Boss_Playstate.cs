2f1c20a8d806a1e9baaca678b15734d3441cc67a
Virgule222 <virgilelions@hotmail.fr>
2014-12-04 11:52:42 +0100

    Mise en commun ();

---
+++ LifeRestore3GDUnity/Assets/Scripts/Scripts Virgile/Boss_Playstate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boss_Playstate : MonoBehaviour {
+
+	public int _Boss_Pv;
+
+	// Use this for initialization
+	void Start () {
+		_Boss_Pv = 1000;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
