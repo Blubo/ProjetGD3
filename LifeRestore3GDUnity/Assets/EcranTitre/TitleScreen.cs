be0c816bb7a2509c4eb2202c15b02a4eab7af504
Virgule222 <virgilelions@hotmail.fr>
2015-01-09 20:09:41 +0100

    no message

---
+++ LifeRestore3GDUnity/Assets/EcranTitre/TitleScreen.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using XInputDotNetPure;
+
+public class TitleScreen : MonoBehaviour {
+	//Xinput
+	bool playerIndexSet = false;
+	public PlayerIndex playerIndex;
+	GamePadState state;
+	GamePadState prevState;
+	
+	private PlayerState _myPlayerState;
+
+	void Awake(){
+		_myPlayerState = GetComponent<PlayerState>();
+	}
+
+	void Update () {
+		//Xinput
+		prevState = state;
+		state = GamePad.GetState(playerIndex);
+
+		//Si le joueur 1 Appuie sur A alors on lance la partie
+		if(prevState.Buttons.A ==ButtonState.Released && state.Buttons.A == ButtonState.Pressed){
+			Application.LoadLevel("Vir_Scene_3DgamesceneCopieLouis");
+		}
+	}
+}
