733b3baf4455303f515f076bf46afd8937c7e327
Virgule222 <virgilelions@hotmail.fr>
2015-01-14 20:04:51 +0100

    PRAY THE SUN

---
+++ LifeRestore3GDUnity/Assets/Presentation/CameraManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraManager : MonoBehaviour {
+	
+	public float v_cameraSpeedTranslate;
+	public int _CurrentSlide;
+	public Transform[] v_myCameras;
+	//public Transform v_PlacementCameras;
+
+	// Use this for initialization
+	void Start () {
+		_CurrentSlide = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Camera.main.transform.position = Vector3.Lerp (Camera.main.transform.position, v_myCameras[_CurrentSlide].position, Time.deltaTime*v_cameraSpeedTranslate*0.9f);
+	}
+
+	void OnTriggerEnter(Collider _collider) {
+		if(_collider.gameObject.tag == "Respawn"){
+			int _index = -1;
+			int.TryParse(_collider.name, out _index);
+			_CurrentSlide = _index;
+		}
+	}
+}
