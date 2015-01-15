73daebcb3f9efe917739e0241c0a8341e403936b
Virgule222 <virgilelions@hotmail.fr>
2015-01-15 04:51:46 +0100

    Commit de la présproto

---
+++ LifeRestore3GDUnity/Assets/Presentation/Scripts/SoundBlock.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundBlock : MonoBehaviour {
+
+	public AudioSource _Song;
+	private bool _playsong;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GetComponent<Sticky> ().v_numberOfLinks != 0) {
+			if (_Song.isPlaying == false) {
+				_Song.Play ();
+			}
+		} else {
+			_Song.Pause ();
+		}
+	}
+}
