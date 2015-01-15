605dc63dc9d1f8a2fa23d9b59f2a6f9bec3382aa
Virgule222 <virgilelions@hotmail.fr>
2015-01-15 19:38:58 +0100

    no message

--- LifeRestore3GDUnity/Assets/Presentation/Scripts/CameraManager.cs
+++ LifeRestore3GDUnity/Assets/Presentation/Scripts/CameraManager.cs
@@ -3,7 +3,12 @@ using System.Collections;
 using XInputDotNetPure;
 
 public class CameraManager : MonoBehaviour {
-	
+
+	bool playerIndexSet = false;
+	public PlayerIndex playerIndex;
+	GamePadState state;
+	GamePadState prevState;
+
 	public float v_cameraSpeedTranslate;
 	public int _CurrentSlide;
 	public Transform[] v_myCameras;
@@ -16,7 +21,20 @@ public class CameraManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		prevState = state;
+		state = GamePad.GetState(playerIndex);
+
 		Camera.main.transform.position = Vector3.Lerp (Camera.main.transform.position, v_myCameras[_CurrentSlide].position, Time.deltaTime*v_cameraSpeedTranslate*0.9f);
+
+		if(prevState.Buttons.Start == ButtonState.Released && state.Buttons.Start == ButtonState.Pressed){
+			_CurrentSlide +=1 ;
+			transform.position = Camera.main.transform.position- new Vector3(-100f,85f);
+		}
+		if(prevState.Buttons.Back == ButtonState.Released && state.Buttons.Back == ButtonState.Pressed){
+			_CurrentSlide -=1 ;
+			transform.position = Camera.main.transform.position- new Vector3(100f,85f);;
+		}
 	}
 
 	void OnTriggerEnter(Collider _collider) {
