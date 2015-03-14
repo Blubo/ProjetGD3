82198971d41452433b8fca86121aa0721bc7f0bc
Virgule222 <virgilelions@hotmail.fr>
2015-03-14 04:16:10 +0100

    scene de l'infiniiiiiiiiiiiiiiiiiii

--- LifeRestore3GDUnity/Assets/Scripts/Scripts Virgile/Modules/Block_Defreeze.cs
+++ LifeRestore3GDUnity/Assets/Scripts/Scripts Virgile/Modules/Block_Defreeze.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Block_Defreeze : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+	    
 	}
 	
 	// Update is called once per frame
@@ -17,6 +18,7 @@ public class Block_Defreeze : MonoBehaviour {
     {
         if (GetComponent<Rigidbody>() == null)
         {
+            gameObject.GetComponent<Renderer>().material.color = Color.green;
             gameObject.AddComponent<Rigidbody>();
             gameObject.layer = LayerMask.NameToLayer("CanBreak");
 
