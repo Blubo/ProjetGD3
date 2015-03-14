960175142b0d5831d774eddbce9d73b74818b45b
Louis Guillemin <louisguillemin@msn.com>
2015-03-14 02:59:36 +0100

    Commit louis: qqs modifs sur bloc spéciaux, prefabétisation pr build les lvls

---
+++ LifeRestore3GDUnity/Assets/Scripts/Scripts Louis/DestroyOnCollision.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOnCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision _coll) {
+		if (_coll.gameObject.layer == 18 || _coll.gameObject.layer == 19) {
+			Debug.Log("at");
+			Destroy(gameObject);
+		}
+	}
+
+}
