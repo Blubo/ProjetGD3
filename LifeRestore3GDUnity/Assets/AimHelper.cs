bf9d4d57e97f4c669bf02fdcfe61d411890b6ba1
Louis Guillemin <louisguillemin@msn.com>
2015-03-23 22:24:35 +0100

    Commit Louis: aimHelper

--- LifeRestore3GDUnity/Assets/AimHelper.cs
+++ LifeRestore3GDUnity/Assets/AimHelper.cs
@@ -10,13 +10,39 @@ public class AimHelper : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		Renderer[] childrenRenderers = gameObject.transform.GetComponentsInChildren<Renderer>();
+
 		if(gameObject.transform.parent.GetComponent<ShootF>()._myHook != null){
-
 			if(gameObject.transform.parent.GetComponent<ShootF>()._myHook.GetComponent<HookHeadF>().GrappedTo!=null){
-				gameObject.transform.GetComponentInChildren<Renderer>().enabled = false;
-			}else{
-				gameObject.transform.GetComponentInChildren<Renderer>().enabled = true;
-
+				foreach (Renderer renderer in childrenRenderers) {
+					if(gameObject.transform.parent.GetComponent<ShootF>().playerIndex==XInputDotNetPure.PlayerIndex.One){
+						renderer.material.color = new Color32(255, 255, 0, 50);
+					}
+					if(gameObject.transform.parent.GetComponent<ShootF>().playerIndex==XInputDotNetPure.PlayerIndex.Two){
+						renderer.material.color = new Color32(255, 0, 0, 50);
+					}
+					if(gameObject.transform.parent.GetComponent<ShootF>().playerIndex==XInputDotNetPure.PlayerIndex.Three){
+						renderer.material.color = new Color32(107, 142, 35, 50);
+					}
+					if(gameObject.transform.parent.GetComponent<ShootF>().playerIndex==XInputDotNetPure.PlayerIndex.Four){
+						renderer.material.color = new Color32(0, 0, 205, 50);
+					}
+				}
+			}
+		}else{
+			foreach (Renderer renderer in childrenRenderers) {
+				if(gameObject.transform.parent.GetComponent<ShootF>().playerIndex==XInputDotNetPure.PlayerIndex.One){
+					renderer.material.color = new Color32(255, 255, 0, 180);
+				}
+				if(gameObject.transform.parent.GetComponent<ShootF>().playerIndex==XInputDotNetPure.PlayerIndex.Two){
+					renderer.material.color = new Color32(255, 0, 0, 180);
+				}
+				if(gameObject.transform.parent.GetComponent<ShootF>().playerIndex==XInputDotNetPure.PlayerIndex.Three){
+					renderer.material.color = new Color32(107, 142, 35, 180);
+				}
+				if(gameObject.transform.parent.GetComponent<ShootF>().playerIndex==XInputDotNetPure.PlayerIndex.Four){
+					renderer.material.color = new Color32(0, 0, 205, 180);
+				}
 			}
 		}
 		   
