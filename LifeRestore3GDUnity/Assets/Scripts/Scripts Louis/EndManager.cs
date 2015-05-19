b0c41a7b6f082f57d4de5bb152ea8f35aa6219dc
Louis Guillemin <louisguillemin@msn.com>
2015-05-19 06:16:05 +0200

    COmmit louis: début de podium avec un score en playerprefs global

--- LifeRestore3GDUnity/Assets/Scripts/Scripts Louis/EndManager.cs
+++ LifeRestore3GDUnity/Assets/Scripts/Scripts Louis/EndManager.cs
@@ -14,7 +14,10 @@ public class EndManager : MonoBehaviour {
 	}
 
 	public void TheEnd(){
-//		PlayerPrefs.SetInt
+		PlayerPrefs.SetInt("ScoreGreen", Camera.main.GetComponent<ScoreManager>().Score_Vert);
+		PlayerPrefs.SetInt("ScoreRed", Camera.main.GetComponent<ScoreManager>().Score_Rouge);
+		PlayerPrefs.SetInt("ScoreBlue", Camera.main.GetComponent<ScoreManager>().Score_Bleu);
+
 	}
 
 }
