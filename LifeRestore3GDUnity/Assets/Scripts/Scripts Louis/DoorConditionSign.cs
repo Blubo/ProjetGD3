3bdd1d70d6d868df4eb71e4fb73a4ff255261728
Louis Guillemin <louisguillemin@msn.com>
2015-05-19 06:13:14 +0200

    Commit louis: scene4A terminée+sign sur les portes

---
+++ LifeRestore3GDUnity/Assets/Scripts/Scripts Louis/DoorConditionSign.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorConditionSign : MonoBehaviour {
+
+	[SerializeField]
+	private float viewRange;
+
+	[SerializeField]
+	private LayerMask myDetectionLayer;
+
+	[SerializeField]
+	private GameObject indication;
+	private Renderer indicationRenderer;
+
+	private OpenTheDoor myOpenTheDoor;
+
+	// Use this for initialization
+	void Start () {
+		myOpenTheDoor = GetComponent<OpenTheDoor>();
+		indicationRenderer = indication.GetComponent<Renderer>();
+		indicationRenderer.enabled = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Detection() == true){
+			if(myOpenTheDoor.closedAlready == true){
+				indicationRenderer.enabled =true;
+			}else{
+				indicationRenderer.enabled =false;
+			}
+		}else{
+			indicationRenderer.enabled =false;
+		}
+	}
+
+	bool Detection(){
+		Collider[] colliders = Physics.OverlapSphere(transform.position, viewRange, myDetectionLayer);
+		foreach(Collider find in colliders){
+
+			if(find != null){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	void ChangeRendererState(Renderer rend){
+		rend.enabled =!rend.enabled;
+	}
+
+}
---
+++ LifeRestore3GDUnity/Assets/Scripts/Scripts Louis/ScoreAwake.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreAwake : MonoBehaviour {
+
+	void Awake(){
+		PlayerPrefs.SetInt("ScoreGreen", 0);
+		PlayerPrefs.SetInt("ScoreRed", 0);
+		PlayerPrefs.SetInt("ScoreBlue", 0);
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
