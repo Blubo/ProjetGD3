70df63417e832b476dea08d7e558f2125cd71b65
Louis Guillemin <louisguillemin@msn.com>
2015-05-19 11:38:04 +0200

    Commit louis: scene de scores+réticules partout+jsais pas quoi

--- LifeRestore3GDUnity/Assets/Scripts/Scripts Louis/Score/ScoreAwake.cs
+++ LifeRestore3GDUnity/Assets/Scripts/Scripts Louis/Score/ScoreAwake.cs
@@ -17,6 +17,11 @@ public class ScoreAwake : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+//		if(Input.GetKeyDown(KeyCode.A)){
+//			Debug.Log("a");
+//			PlayerPrefs.SetInt("ScoreGreen", 100);
+//			PlayerPrefs.SetInt("ScoreRed", 60);
+//			PlayerPrefs.SetInt("ScoreBlue", 50);
+//		}
 	}
 }
