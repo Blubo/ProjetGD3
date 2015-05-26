f8e1baac9820cd0097c8793b7858c07b25883343
Virgule222 <virgilelions@hotmail.fr>
2015-05-26 19:31:51 +0200

    changements camera

--- LifeRestore3GDUnity/Assets/NewCameraSystem/VirgileTest/DynamicCamera.cs
+++ LifeRestore3GDUnity/Assets/NewCameraSystem/VirgileTest/DynamicCamera.cs
@@ -24,31 +24,52 @@ public class DynamicCamera : MonoBehaviour {
 
 	void Update () {
     CalculateCenter(_Idole.transform, _Players[0].transform, _Players[1].transform, _Players[2].transform);
+    FieldOfVision();
     CameraPosition();
 	}
 
   void CameraPosition()
   {
-    Transform tempPosition = null;
+   /* Transform tempPosition = null;
     if(GetFarthestElementFrom() <= 8){
       tempPosition = _Mini;
+      MainCamera.fieldOfView = 75;
     }
     else if (GetFarthestElementFrom() <= 25)
     {
       tempPosition = _Medium;
+      MainCamera.fieldOfView = 85;
     }
     else 
     {
       tempPosition = _High;
-    }
-    transform.position =  Vector3.Lerp(transform.position, new Vector3( _Target.transform.position.x,(_Mini.position.y+ GetFarthestElementFrom()), _Target.transform.position.z),0.9f *Time.deltaTime);
+      MainCamera.fieldOfView = 90;
+    }*/
+
+    transform.position =  Vector3.Lerp(transform.position, new Vector3( _Target.transform.position.x,(_Mini.position.y+ GetFarthestElementFrom())* 1.3f, _Target.transform.position.z-10.0f),0.9f *Time.deltaTime);
   }
 
   void CalculateCenter(Transform A, Transform B, Transform C, Transform D)
   {
-    Vector3 MidPoint = (A.position + B.position + C.position + D.position);
-    MidPoint = MidPoint / 4;
+    Vector3 MidPoint = (A.position + B.position + C.position + D.position+A.position + A.position);
+    MidPoint = MidPoint / 6;
     _Target.position = MidPoint;
+  }
+
+  void FieldOfVision()
+  {
+    if (MainCamera.transform.position.y <= 30)
+    {
+     // MainCamera.fieldOfView = Mathf.Lerp(MainCamera.fieldOfView, 60, 1.0f * Time.deltaTime) ;
+    }
+    else if (MainCamera.transform.position.y <= 50)
+    {
+     /// MainCamera.fieldOfView = Mathf.Lerp(MainCamera.fieldOfView, 60, 1.0f * Time.deltaTime);
+    }
+    else if (MainCamera.transform.position.y <= 60)
+    {
+      //MainCamera.fieldOfView = Mathf.Lerp(MainCamera.fieldOfView, 60, 1.0f * Time.deltaTime);
+    }
   }
 
   float GetFarthestElementFrom()
