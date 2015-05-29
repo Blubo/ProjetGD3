9ce9e71e482d4461e0f6b9de23f6c7ecd664dc3c
Virgule222 <virgilelions@hotmail.fr>
2015-05-29 03:39:13 +0200

    Ajout de la scène LD RE

---
+++ LifeRestore3GDUnity/Assets/BecomeMovable.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BecomeMovable : MonoBehaviour {
+
+  public GameObject _NewBloc;
+
+  void Activated()
+  {
+    gameObject.GetComponent<BoxCollider>().enabled = false;
+    _NewBloc.transform.position = gameObject.transform.position;
+    Destroy(gameObject);
+  }
+}
