d8968f2ef9c01b3830ff4adbe9f4625b2f4e3f0b
Virgule222 <virgilelions@hotmail.fr>
2015-05-29 17:13:05 +0200

    a

---
+++ LifeRestore3GDUnity/Assets/Scripts/Scripts Louis/TitleScreenScripts/GeneralScoreHUD.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GeneralScoreHUD : MonoBehaviour {
+	
+	private GeneralScoreManager generalScoreManager;
+
+	public GameObject Vert, Bleu, Rouge;
+	private Text _Vert, _Bleu, _Rouge;
+	
+	// Use this for initialization
+	void Start () {
+		generalScoreManager = Camera.main.GetComponent<GeneralScoreManager>();
+		
+		_Vert = Vert.GetComponent<Text>();
+		_Rouge = Rouge.GetComponent<Text>();
+		_Bleu = Bleu.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_Vert.text = "" + generalScoreManager.generalGreen;
+		_Rouge.text = "" + generalScoreManager.generalRed;
+		_Bleu.text = "" + generalScoreManager.generalBlue;
+	}
+}
